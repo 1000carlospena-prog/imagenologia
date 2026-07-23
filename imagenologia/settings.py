72074af0085ca8ab4f9f4ae9ffde56ab5f35cd66
Carlos <1000carlos.pena@gmail.com>
2026-07-23 07:23:16 -0400

    fix: remove redundant STATICFILES_DIRS causing duplicate collection warnings

--- imagenologia/settings.py
+++ imagenologia/settings.py
@@ -79,7 +79,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'inventario' / 'static']
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
